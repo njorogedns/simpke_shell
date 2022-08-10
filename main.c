7d803331d7c09d33101819ddd57acfe88ee48994
Njorogedns <njorogedns13@gmail.com>
2022-08-10 10:48:15 -0700

    comments

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * main - runs the shell program
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
   char **av, *PATH = NULL, *fullpathbuffer = NULL, *copy = NULL, *buffer = NULL;
--- strtok.c
+++ strtok.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * _memcpy - copies information between void pointers.
+ * @newptr: destination pointer.
+ * @ptr: source pointer.
+ * @size: size of the new pointer.
+ *
+ * Return: no return.
+ */
 void _memcpy(void *newptr, const void *ptr, unsigned int size)
 {
   char *char_ptr = (char *)ptr;
@@ -9,10 +16,7 @@ void _memcpy(void *newptr, const void *ptr, unsigned int size)
   for (i = 0; i < size; i++)
     char_newptr[i] = char_ptr[i];
 }
-
-
-
-
+/*returns the next token in a C-string*/
 char *_strtok(char str[], const char *delim)
 {
   static char *splitted, *str_end;
